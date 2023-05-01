b94181ebbb6942da97d6cf0d960c1a1a22fd1adb
Andrei Mititelu <andrei.mititelu@tradingcentral.com>
2023-05-02 01:49:39 +0300

    added Guest constructor with all fields as parameters

--- src/Guest.java
+++ src/Guest.java
@@ -5,4 +5,11 @@ public class Guest {
     private String firstName;
     private String email;
     private String phoneNumber;
+
+    public Guest(String lastName, String firstName, String email, String phoneNumber){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 }
